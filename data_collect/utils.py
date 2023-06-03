4a34aab7815750201fb57f5669c75d4bfc05549a
Rafael Santana <jrafaeldesantana@gmail.com>
2023-06-03 10:22:38 -0300

    update sns topic

--- data_collect/utils.py
+++ data_collect/utils.py
@@ -1,8 +1,6 @@
 import os
 import OpenOPC
 import boto3
-import random
-import string
 
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
@@ -10,11 +8,6 @@ def cls():
 def printInfos(interation, time_start):
   print("Captura iniciada as: " + time_start)
   print(str(interation) + ' novas linhas enviadas para a fila.')
-
-def get_random_string(length):
-  letters = string.ascii_lowercase
-  result_str = ''.join(random.choice(letters) for i in range(length))
-  return result_str
 
 def connectOpc(opc_server, opc_host):
   opc = OpenOPC.client()
@@ -24,11 +17,6 @@ def connectOpc(opc_server, opc_host):
 
 def publishMessageToSns(topic_name, region_name, message):
    sns_client = boto3.client("sns", region_name)
-   message_id = sns_client.publish(
-      TopicArn=topic_name, 
-      Message=message, 
-      MessageGroupId='group',
-      MessageDeduplicationId=get_random_string(16)
-    )
+   message_id = sns_client.publish(TopicArn=topic_name, Message=message)
 
    return message_id
