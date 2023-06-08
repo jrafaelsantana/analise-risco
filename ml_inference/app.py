9af2119374dd614f389f023bd091f9a31ad01eb3
Rafael Santana <jrafaeldesantana@gmail.com>
2023-06-07 21:43:17 -0300

    update model

--- ml_inference/app.py
+++ ml_inference/app.py
@@ -1,4 +1,5 @@
 import os
+import joblib
 import torch
 import numpy as np
 import pandas as pd
@@ -16,19 +17,46 @@ mongoClient = MongoClient(MONGODB_URI)
 sensorDataDb = mongoClient.sensor_data
 historicTable = sensorDataDb.historic
 
-liquid_level_model = NeuralNet()
+liquid_level_model = NeuralNet(50)  # TODO: Refactor to pass num_features
 liquid_level_model.load_state_dict(torch.load(Path('./models/liquid_level', 'model.pt')))
 liquid_level_model.eval()
+liquid_level_scaler = joblib.load(Path('./models/liquid_level', 'scaler.gz'))
+
+liquid_level_blocked_features = [
+    'Drum.V1.VOL',
+    'Valvula.VALVE_SHUTOFF.CL',
+    'Valvula.VALVE_SHUTOFF.DP',
+    'Valvula.VALVE_SHUTOFF.OP',
+    'Variaveis.TR1_RUIDO',
+    'Variaveis.TR2_RUIDO',
+    'Variaveis.TR3_RUIDO',
+    'Tubo.S6.T',
+    'Tubo.S6.P',
+    'Tubo.S6.W',
+    'Tubo.S25.T',
+    'Tubo.S25.P',
+    'Tubo.S25.W',
+    'Valvula.VALVE4.CL',
+    'Valvula.VALVE4.OP',
+    'StreamSend.SAIDA_LIQUIDO.P'
+]
 
 
 @app.route('/liquid_level', methods=['GET'])
 def predict_liquid_level():
   lastRecord = historicTable.find().sort('_id', -1 ).limit(1)
   list_record = list(lastRecord)
+  print(list_record)
 
   df = pd.DataFrame(list_record)
   df = df.drop('_id', axis=1)
   df = df.drop('Datetime', axis=1)
+  df = df.drop(liquid_level_blocked_features, axis=1)
+  
+  df['Label'] = 0
+  df = pd.DataFrame(liquid_level_scaler.transform(df), index=df.index, columns=df.columns)
+  df = df.drop('Label', axis=1)
+  
   
   features = torch.Tensor(np.array(df))
   output = liquid_level_model(features)
--- ml_inference/models/liquid_level/model.py
+++ ml_inference/models/liquid_level/model.py
@@ -1,16 +1,27 @@
 import torch.nn as nn
 
-class NeuralNet(nn.Module):
-  def __init__(self):
-    super(NeuralNet, self).__init__()
-    self.fc1 = nn.Linear(66, 64)
-    self.fc2 = nn.Linear(64, 32)
-    self.fc3 = nn.Linear(32, 1)
-    self.relu = nn.ReLU()
-    self.sigmoid = nn.Sigmoid()
+# class NeuralNet(nn.Module):
+#   def __init__(self, num_features):
+#     super(NeuralNet, self).__init__()
+#     self.fc1 = nn.Linear(num_features, 64)
+#     self.fc2 = nn.Linear(64, 32)
+#     self.fc3 = nn.Linear(32, 1)
+#     self.relu = nn.ReLU()
+#     self.sigmoid = nn.Sigmoid()
 
-  def forward(self, x):
-    x = self.relu(self.fc1(x))
-    x = self.relu(self.fc2(x))
-    x = self.sigmoid(self.fc3(x))
-    return x
+#   def forward(self, x):
+#     x = self.relu(self.fc1(x))
+#     x = self.relu(self.fc2(x))
+#     x = self.sigmoid(self.fc3(x))
+#     return x
+
+class NeuralNet(nn.Module):
+    def __init__(self, num_features):
+        super(NeuralNet, self).__init__()
+        self.fc = nn.Linear(num_features, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        output = self.fc(x)
+        output = self.sigmoid(output)
+        return output
