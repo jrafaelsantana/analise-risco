5dd6cb71ea2ed001818c7fdea867dfaa46771952
Rafael Santana <jrafaeldesantana@gmail.com>
2023-10-15 10:28:58 -0300

    refactor main read

--- knockout_ml/read/main.py
+++ knockout_ml/read/main.py
@@ -14,35 +14,36 @@ context = zmq.Context()
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
 
+buffer = []
+request = 0
+
 def close_connection(socket, context):
     socket.close()
     context.term()
 
-buffer = []
-request = 0
+if __name__ == '__main__':
+    while True:
+        try:
+            print("Reading simulation %s …" % request)
 
-while True:
-    try:
-        print("Reading simulation %s …" % request)
+            read_opc = opc.read(TAGS_READ)
+            data = [tag[1] for tag in read_opc]
+            buffer.append(data)
+            
+            if len(buffer) >= 30:
+                socket.send_pyobj(buffer[-30:])
 
-        read_opc = opc.read(TAGS_READ)
-        data = [tag[1] for tag in read_opc]
-        buffer.append(data)
-        
-        if len(buffer) >= 2:
-            socket.send_pyobj(buffer[-30:])
-
-            message = socket.recv()
-            received = handle_message(message)
-            print("Received [ %s ]" % (received))
-        
-        time.sleep(READ_SECONDS)
-        request = request + 1
-    except SocketClosedError:
-        close_connection(socket, context)
-        print("Killed.")
-        exit()
-    except:
-        socket.send(b"CLOSE_SOCKET")
-        close_connection(socket, context)
-        exit()
+                message = socket.recv()
+                received = handle_message(message)
+                print("Received [ %s ]" % (received))
+            
+            time.sleep(READ_SECONDS)
+            request = request + 1
+        except SocketClosedError:
+            close_connection(socket, context)
+            print("Killed.")
+            exit()
+        except:
+            socket.send(b"CLOSE_SOCKET")
+            close_connection(socket, context)
+            exit()
